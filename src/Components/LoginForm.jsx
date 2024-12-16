6b913a829e3776d73ccc67789167366a11eaf225
Helena Penha <lenapenha02@hotmail.com>
2024-12-16 16:41:25 -0600

    add message validation for error and success

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,60 +1,92 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import './LoginForm.css'
-import { FaEnvelope , FaUser, FaLock } from 'react-icons/fa';
+import { FaEnvelope, FaUser, FaLock } from 'react-icons/fa';
 
 const LoginForm = () => {
     const navigate = useNavigate();
-    const [action, setAction] = useState("Sign in")
+    const [action, setAction] = useState("Sign in");
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    
+    const [error, setError] = useState(null);
+    const [message, setMessage] = useState(null);
+
+    const handleSignUpClick = () => {
+        setError(null);
+        setMessage(null);
+        setAction("Sign up");
+    }
+
+    const handleSignInClick = () => {
+        setError(null);
+        setMessage(null);
+        setAction("Sign in");
+    }
 
     async function login() {
-        let item = { username, password }
-        let result = await fetch(`${process.env.REACT_APP_API_HOST}/auth/login`, {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(item)
-        });
+        setError(null);
+        try {
+            let item = { username, password }
+            let response = await fetch(`${process.env.REACT_APP_API_HOST}/auth/login`, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(item)
+            });
 
-        result = await result.json()
-        if (result.token) {
-            localStorage.setItem("jwt", result.token)
-            navigate("/me");
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.details || 'Something went wrong, please try again');
+            }
+
+            const result = await response.json();
+            if (result.token) {
+                localStorage.setItem("jwt", result.token)
+                navigate("/me");
+            }
+        } catch (err) {
+            setError(err.message);
         }
+
     }
 
     async function signUp() {
-        let item = { email, username, password };
-        console.log(item);
-        let result = await fetch(`${process.env.REACT_APP_API_HOST}/auth/new`, {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(item)
-        });
+        setError(null);
+        try {
+            let item = { email, username, password };
+            let response = await fetch(`${process.env.REACT_APP_API_HOST}/auth/new`, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(item)
+            });
 
-        result = await result.json();
-        console.log(result);
-        setAction("Sign in");
+            if (response.status !== 201) {
+                const errorData = await response.json();
+                throw new Error(errorData.details || 'Something went wrong, please try again');
+            }
+            setMessage("User created successfully")
+            setAction("Sign in");
+        } catch (err) {
+            setError(err.message);
+        }
+
     }
 
     return (
         <div className="wrapper">
 
             <h1>{action}</h1>
-            {action==="Sign in"?<div></div>:
-                 <div className="input-box">
+            {action === "Sign in" ? <div></div> :
+                <div className="input-box">
                     <input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
                     <FaEnvelope className="icon" />
                 </div>
             }
-           
+
             <div className="input-box">
                 <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} required />
                 <FaUser className="icon" />
@@ -65,16 +97,23 @@ const LoginForm = () => {
             </div>
 
             <button onClick={action === "Sign in" ? login : signUp} >Send</button>
-            
-            {action==="Sign up"?<div></div>:
+
+            <div className="submit-container">
+                <div className="error">{error}</div>
+            </div>
+            <div className="submit-container">
+                <div className="success-message">{message}</div>
+            </div>
+
+            {action === "Sign up" ? <div></div> :
                 <div className="submit-container">
-                    <div className="create-account">Don´t have an account?<span onClick={() => setAction("Sign up")}> Sign up</span></div>
+                    <div className="create-account">Don´t have an account?<span onClick={() => handleSignUpClick()}> Sign up</span></div>
                 </div>
             }
-            
-            {action==="Sign in"?<div></div>:
+
+            {action === "Sign in" ? <div></div> :
                 <div className="submit-container">
-                    <div className="create-account">Already have an account?<span onClick={() => setAction("Sign in")}> Sign in</span></div>
+                    <div className="create-account">Already have an account?<span onClick={() => handleSignInClick()}> Sign in</span></div>
                 </div>
             }
         </div>
